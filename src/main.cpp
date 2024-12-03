0b1caf4aa0315d7d3c620f4c3afa16f6145ae320
wjl <wjl16021031@gmail.com>
2024-12-03 20:05:38 +0800

    修复bug

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,6 @@
 int main(int argc, char* argv[]){
     SINFO << "It's an info log messagetest.";
     SERROR << "It's an error log message test!";
+    SINFO << "It's an info dsada messagetest.";
     return 0;
 }
